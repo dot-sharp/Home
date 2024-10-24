a81d935f1e5249662aa9dab607627a44efccaeba
Casparml <casparmlandman@gmail.com>
2024-10-24 11:51:06 +0200

    [/] Hotfix

---
+++ assets/js/scripts.js
@@ -0,0 +1,20 @@
+// Dynamic Favicon
+window.onload = function () {
+  const favicon = document.getElementById('favicon')
+
+  document.addEventListener('visibilitychange', function (e) {
+    const isPageActive = !document.hidden
+    toggle(isPageActive)
+  })
+
+  function toggle(isPageActive) {
+    if (isPageActive) {
+      favicon.href = 'assets/images/logo.png'
+    } else {
+      favicon.href = 'assets/images/logo-away.png'
+    }
+  }
+}
+
+// Get current year
+document.getElementById("currentYear").innerHTML = (new Date().getFullYear());
